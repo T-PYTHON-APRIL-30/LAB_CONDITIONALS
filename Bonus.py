1abc61abb960ee28c23bca7f2505688364f7913d
Yousef M <yousefmalsaadan@gmail.com>
2023-05-03 13:28:39 +0300

    update the bouns soltion

--- Bonus.py
+++ Bonus.py
@@ -1,11 +1,11 @@
 name = input("Your Name: ")
 email = input("Your Email: ")
 
-if email.find("@gmail.com") == -1 or email.find(" ") != -1:
+if not email.endswith("@gmail.com") or email.find(" ") != -1 or email.count("@") == 1:
     print("the email is not valid , please provide a valid email")
 elif len(name) <= 2:
     print("the name length must be more than 2 characters, please provide a valid name.")
-elif len(name) > 2 and email.find("@gmail.com") != -1:
+elif len(name) > 2 and email.endswith("@gmail.com"):
     print(f"welcome {name}, you registered with the email {email} !")
 else:
     print("something wrong!!")
