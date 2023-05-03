e009076f0b87dc83176c1cd689c5e1c757c56866
juhaina <jh75n.awad@gmail.com>
2023-05-03 11:34:25 +0300

    Solve lab

---
+++ condition.py
@@ -0,0 +1,20 @@
+namemovie = "scream"
+reat = 5
+reatmovie = 3
+score = 72.65
+
+
+if reatmovie >= 4 and score > 80:
+    print("Highly recommended")
+
+elif reatmovie >= 3 and score > 70:
+    print("I recommended it . It is good")
+
+elif reatmovie <= 2 and score > 60:
+    print("You should check it out!")
+
+
+else:
+    print("Don't watch it. It is a waste of time")
+
+    
