b17b53da39c957dcb4a9430bbc7b7515c7a974f9
MK <MK.97@outlook.sa>
2023-05-03 12:00:30 +0300

    solved the bonus

--- bonus.py
+++ bonus.py
@@ -4,12 +4,12 @@ userName= input ("enter your name: ")
 email= input ("enter you email: ")
 
 
-if len(userName) < 2:
+if len(userName) < 3:
     validLogin = False
 elif "@gmail.com" not in email or " " in email:
     validLogin = False
 
-if validLogin:
+if validLogin == True:
     print("welcome {}, you registered with the email {} !".format(userName,email))
 else:
     print("the email is not valid , please provide a valid email")
