ecddf0d9698c71b48426bb99cf363435b865d29a
khalid <10abha10@gmail.com>
2023-05-03 11:32:28 +0300

    solved the lab

---
+++ main.py
@@ -0,0 +1,13 @@
+movie_name="saw III"
+movie_rate=3
+popularity=72.65
+if movie_rate >=4 and popularity>=80:
+    print("Highly recommended")
+elif movie_rate >=3 and popularity >=70:
+    print("I recommended it . It is good")
+elif movie_rate>=2 and popularity>=60:
+   print("You should check it out!")
+else:
+    print("Don't watch it. It is a waste of time")
+ 
+
