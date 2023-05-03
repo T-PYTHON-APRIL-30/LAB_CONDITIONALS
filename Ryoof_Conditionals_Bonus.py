ce93d94b5e1e08e1007bad7d6517da06ad31a8ae
ryuof alarfaj <ryuofalarfaj@gmail.com>
2023-05-03 13:08:24 +0300

    Ryoof_Conditionals

---
+++ Ryoof_Conditionals_Bonus.py
@@ -0,0 +1,29 @@
+
+# ## Bonus
+# ### write a program that asks the user to provide his name and his email using input 
+# , then do the following:
+# - Check that the name length is more than 2 characters.
+# - Check that the email is valid (using string operations and coditionals)
+# - You only accept @gmail emails . 
+# - if the email is valid, display a welcome message to the user . for example :
+
+# welcome Ahmed, you registered with the email ahmed@gmail.com !
+
+# ```
+# - if the email or name is not valid, display the message : 
+# ```
+#  the email is not valid , please provide a valid email .
+# ```
+
+# or 
+
+# ```
+# the name length must be more than 2 characters, please provide a valid name.
+# ```
+
+# write a program that asks the user to provide his name and his email using input 
+user_name=input("Please Enter your Name:")
+user_email=input("Please Enter your email:")
+
+
+
