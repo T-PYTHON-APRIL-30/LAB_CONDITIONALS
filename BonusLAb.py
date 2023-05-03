b4c21dff357a5a57157afdf9da3dda341909604c
amani <amanii1994@outlook.com>
2023-05-03 12:07:47 +0300

    Edited the bonus BonusLAb

--- BonusLAb.py
+++ BonusLAb.py
@@ -20,7 +20,7 @@ email = input('Please enter your email: ')
 if len(name) > 2:
 
     if email.find('@gmail.com') > -1:
-        print(f'Welcome {name}, ou registered with the email {email}!')
+        print(f'Welcome {name}, you registered with the email {email}!')
 
     else:
         print('the email is not valid , please provide a valid email!!')
