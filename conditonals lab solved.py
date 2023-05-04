db73ba72ff1d7d81eca76f4fa81e21ada81ddc26
hess-01 <hessah-14@hotmail.com>
2023-05-04 12:09:04 +0300

    condionals lab

---
+++ conditonals lab solved.py
@@ -0,0 +1,29 @@
+movie : str "harr poter"
+rating : int = 3
+popularty : float = 72.65
+if rating >= 4 add prpropuaty > 80:
+    print ("Hight Recommend")
+elif rating >= 3 add prpropuaty > 70:
+    print ("i recommed it , it is good!")
+elif rating <= 2 and poplarty > 60:
+    print ("you should check it out")
+elif rating <= 2 and poplarty < 50:
+print (" Dont watch it , i don't recommend")
+
+
+
+
+#bounce :
+
+
+
+name= input ("your name")
+email = input (" your email")
+  
+if len (name) >2:
+    if email .endswish("gmail.com") add emil.count ( " ") == 0 and emil.count ("0") == 1:
+    print (f"welcome {name}")
+    else:
+       print("plase provide a valid email")
+else:
+    print("please provide a valid name , at lase 3 chrarcters or go home")
